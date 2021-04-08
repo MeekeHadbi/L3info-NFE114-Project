cc75bddb39a22d0aeaaa9bf6d9e89fa6db4126db
andreC125 <andre125.cespedes@gmail.com>
2021-04-08 09:25:18 +0200

    ajout fenetre login

--- vaccinaction_server.py
+++ vaccinaction_server.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 from flask import g
 from datetime import datetime, timedelta
 import time
@@ -6,10 +9,21 @@ import time
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'vaccinaction'
+
+# Intialize MySQL
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/connexion')
+def login():
+    return render_template("connexion.html")
 
 
 if __name__ == '__main__':
