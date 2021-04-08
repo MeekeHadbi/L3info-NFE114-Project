71c0b4276f2c91a7c0a136477acaa46c17ff4f35
Stan Racz <stan.racz7@gmail.com>
2021-04-08 09:32:36 +0200

    creation nouvelle branche connexion base de donnees

--- vaccinaction_server.py
+++ vaccinaction_server.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask import g
 from datetime import datetime, timedelta
 import time
-
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'vaccinaction'
+
+mysql = MySQL()
+mysql.init_app(app)
+
+def get_all_appointments():
+    
+    cursor = mysql.get_db().cursor()
+    datas = cursor.execute("select * from data_poseidon ORDER BY id DESC")
+    str(datas)
+    return datas
+
+def connect_db():
+    
+    return connection
 
 @app.route('/')
 def index():
